2051aa9f056b07b5010b0f64a06cca5aab4e9910
vai-xyz <119789706+vai-xyz@users.noreply.github.com>
2023-07-22 15:49:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+video="";
+status="";
+input="";
+
+function setup(){
+    canvas=createCanvas(360,360);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(360,360);
+    video.hide();
+}
+
+function draw(){
+   image(video,0,0,360,360);
+}
+
+function start(){
+    objectDetector= ml5.objectDetector('cocussd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Object Detecting";
+    input=docuemnt.getElementById("input_id").value;
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+}
